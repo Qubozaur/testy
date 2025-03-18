25d3a66d5dec47600b1f607ab1db52a4e7d1fae5
Qubozaur <stolarczykjakub25@gmail.com>
2025-03-18 18:05:17 +0100

    'seia'

---
+++ testy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace testy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
